f772b61e7b3d2ed312ad89daebc85c2ea8ed3f03
kronnor919 <segundacuenta7364@gmail.com>
2025-12-25 01:13:50 -0500

    feat(app): agregar y configurar el servidor flask

---
+++ src/app.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from flask import Flask
+from routes import register_routes
+from db import init_db
+from common import SQLITE_DB_URI
+
+
+def create_app() -> Flask:
+    app = Flask(__name__, instance_relative_config=True)
+    
+    Path(app.root_path, "instance").mkdir(exist_ok=True)
+    
+    if not app.config.from_pyfile("config.py", silent=True):
+        app.config.from_mapping({
+            "SERVER_NAME": "localhost:5000",
+            "DEBUG": True,
+            "SQLALCHEMY_DATABASE_URI": SQLITE_DB_URI,
+            "SQLALCHEMY_ECHO": True
+        })
+    
+    init_db(app)
+    register_routes(app)
+    
+    return app
