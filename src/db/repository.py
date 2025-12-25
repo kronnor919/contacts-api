e16032c5bd9809396dbd03732aff310e4baf9ce1
kronnor919 <segundacuenta7364@gmail.com>
2025-12-25 01:04:13 -0500

    feat(db): agregar repositorio de base de datos (operaciones CRD)

---
+++ src/db/repository.py
@@ -0,0 +1,44 @@
+from typing import List
+from .db import get_db
+from .models import ContactModel
+from entities import Contact
+
+
+class ContactsRepository:
+    def __init__(self) -> None:
+        self.db = get_db()
+    
+    def all(self) -> List[Contact] | List[None]:
+        users = self.db.query(ContactModel).all()
+        
+        return [u.to_entity() for u in users]
+    
+    def get(self, id: int) -> Contact | None:
+        user = self.db.get(ContactModel, id)
+        
+        if not user:
+            return None
+        
+        return user.to_entity()
+    
+    def add(self, c: Contact) -> Contact:
+        m = ContactModel(
+            id=c.id,
+            tag=c.tag,
+            phone=c.phone,
+            created_at=c.created_at
+        )
+        self.db.add(m)
+        self.db.commit()
+        
+        return m.to_entity()
+
+    def delete(self, id: int) -> Contact | None:
+        e = self.db.get(ContactModel, id)
+        
+        if not e:
+            return None
+        
+        self.db.delete(e)
+        self.db.commit()
+        return e.to_entity()
